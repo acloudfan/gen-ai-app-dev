f020c0b765c575f2b13ce6bae5897e9c5250125b
acloudfan <raj@acloudfan.com>
2024-07-21 17:05:37 -0400

    agents

--- utils/create_chat_llm.py
+++ utils/create_chat_llm.py
@@ -1,6 +1,10 @@
 from langchain_cohere import ChatCohere
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_community.llms import HuggingFaceEndpoint, ChatHuggingFace
+
+https://api.python.langchain.com/en/latest/chat_models/langchain_huggingface.chat_models.huggingface.ChatHuggingFace.html
+from langchain_huggingface import ChatHuggingFace
 
 
 def create_cohere_chat_llm(args={}):
@@ -23,3 +27,16 @@ def create_gpt_chat_llm(args={}):
     llm = ChatOpenAI(**args) 
     return llm
 
+# HuggingFace
+## Create a hugging face model
+## Default model = 
+def create_hugging_face_chat_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={}):
+    
+    llm = HuggingFaceEndpoint(
+        repo_id = repo_id,
+        **args
+    )
+
+    chat_llm = ChatHuggingFace(llm=llm, verbose=True)
+    
+    return chat_llm
