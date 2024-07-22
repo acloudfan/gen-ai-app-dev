ec06eb203c9816c63370ed6a33dc7ddc49371b6a
acloudfan <raj@acloudfan.com>
2024-07-22 15:21:08 -0400

    agents

--- utils/create_chat_llm.py
+++ utils/create_chat_llm.py
@@ -1,10 +1,11 @@
 from langchain_cohere import ChatCohere
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-from langchain_community.llms import HuggingFaceEndpoint, ChatHuggingFace
+# from langchain_community.llms import HuggingFaceEndpoint, ChatHuggingFace
 
-https://api.python.langchain.com/en/latest/chat_models/langchain_huggingface.chat_models.huggingface.ChatHuggingFace.html
-from langchain_huggingface import ChatHuggingFace
+# !pip install -U langchain-huggingface
+# https://api.python.langchain.com/en/latest/chat_models/langchain_huggingface.chat_models.huggingface.ChatHuggingFace.html
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 
 def create_cohere_chat_llm(args={}):
@@ -30,13 +31,13 @@ def create_gpt_chat_llm(args={}):
 # HuggingFace
 ## Create a hugging face model
 ## Default model = 
-def create_hugging_face_chat_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={}):
+def create_hugging_face_chat_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={}, verbose=False):
     
     llm = HuggingFaceEndpoint(
         repo_id = repo_id,
         **args
     )
 
-    chat_llm = ChatHuggingFace(llm=llm, verbose=True)
+    chat_llm = ChatHuggingFace(llm=llm, verbose=verbose)
     
     return chat_llm
