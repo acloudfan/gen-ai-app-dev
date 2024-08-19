069e76a1df84e5fb95cf94d986bddd9d7b743ed3
acloudfan <raj@acloudfan.com>
2024-08-19 10:13:57 -0400

    ok

--- Setup/gcsetup.sh
+++ Setup/gcsetup.sh
@@ -7,4 +7,7 @@ echo "Creating utils folder ..."
 mv ./gen-ai-app-dev/utils ./utils
 rm -rf gen-ai-app-dev
 
+echo "installing common packages ..."
+pip install matplotlib=3.8.4  python-dotenv=0.21.0 
+
 echo "Done"
