8f5409c8d8e19814de2716e6f0506eb603523674
acloudfan <raj@acloudfan.com>
2024-08-19 10:01:35 -0400

    ok

--- Setup/gcsetup.sh
+++ Setup/gcsetup.sh
@@ -1,5 +1,10 @@
 !bash
 
-git clone https://github.com/acloudfan/gen-ai-app-dev.git
+echo "Getting the setup script ..."
+git clone https://github.com/acloudfan/gen-ai-app-dev.git --quiet
+
+echo "Creating utils folder ..."
 mv ./gen-ai-app-dev/utils ./utils
 rm -rf gen-ai-app-dev
+
+echo "Done"
