097a5e4059bf2ad8b88eff3c00c25c059df2295e
acloudfan <raj@acloudfan.com>
2024-08-19 10:41:55 -0400

    api_key_check

---
+++ Gen-AI-Fundamentals/gcsetup.sh
@@ -0,0 +1,13 @@
+!bash
+
+echo "Getting the setup script ..."
+git clone https://github.com/acloudfan/gen-ai-app-dev.git --quiet
+
+echo "Creating utils folder ..."
+mv ./gen-ai-app-dev/utils ./utils
+rm -rf gen-ai-app-dev
+
+echo "installing common packages ..."
+pip install --quiet matplotlib==3.8.4  python-dotenv==0.21.0 
+
+echo "Done"
