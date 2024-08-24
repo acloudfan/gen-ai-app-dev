eee2d8a43e7254f878636913e6bf5fc79749705e
acloudfan <raj@acloudfan.com>
2024-08-24 07:52:35 -0400

    lcel demo

--- utils/create_llm.py
+++ utils/create_llm.py
@@ -5,6 +5,8 @@ from langchain.llms import Cohere
 
 
 import os
+import sys
+
 
 from langchain_openai import OpenAI
 
@@ -16,18 +18,31 @@ from langchain_huggingface import HuggingFaceEndpoint
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import GoogleGenerativeAI
 
+from utils.api_key_check_utility import api_key_check
+
 # Cohere
 ## Create the Cohere model
 
 # https://python.langchain.com/v0.2/docs/integrations/providers/cohere/
-def create_cohere_llm(args={}):
+def create_cohere_llm(args={}, api_key_prompt=False):
+
+    # Check availability in eviroment variable, if not found prompt 
+    if api_key_prompt:
+        api_key = api_key_check("COHERE_API_KEY")
+    
     llm = Cohere(**args) 
     return llm
 
 
 # OpenAI
 ## Create the OpenAI model
-def create_gpt_llm(args={}):
+def create_gpt_llm(args={}, api_key_prompt=False):
+
+    # Check availability in eviroment variable, if not found prompt 
+    if api_key_prompt:
+        api_key = api_key_check("OPENAI_API_KEY")
+
+    
     llm = OpenAI(**args) 
     return llm
 
@@ -35,14 +50,24 @@ def create_gpt_llm(args={}):
 
 # AI21
 ## Create the AI21 Jurassic model
-def create_ai21_llm(args={}):
+def create_ai21_llm(args={}, api_key_prompt=False):
+
+    if api_key_prompt:
+        api_key = api_key_check("AI21_API_KEY")
+        
+    
     llm = AI21(**args)
     
     return llm
 
 # Anthropic
 ## Create the Anthropic Claude models
-def create_anthropic_llm(args={}):
+def create_anthropic_llm(args={}, api_key_prompt=False):
+
+    # Check availability in eviroment variable, if not found prompt 
+    if api_key_prompt:
+        api_key = api_key_check("ANTHROPIC_API_KEY")
+    
     llm = AnthropicLLM(**args)
 
     return llm
@@ -50,8 +75,12 @@ def create_anthropic_llm(args={}):
 # HuggingFace
 ## Create a hugging face model
 ## Default model = 
-def create_hugging_face_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={}):
+def create_hugging_face_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={}, api_key_prompt=False):
     
+    # Check availability in eviroment variable, if not found prompt 
+    if api_key_prompt:
+        api_key = api_key_check("HUGGINGFACEHUB_API_TOKEN")
+        
     llm = HuggingFaceEndpoint(
         repo_id = repo_id,
         **args
@@ -62,8 +91,12 @@ def create_hugging_face_llm(repo_id="mistralai/Mistral-7B-Instruct-v0.2", args={
 # Google AI API
 # https://api.python.langchain.com/en/latest/llms/langchain_google_genai.llms.GoogleGenerativeAI.html
 # pip install --upgrade --quiet  langchain-google-genai
-def create_google_llm(model='gemini-1.5-flash', args={}):
+def create_google_llm(model='gemini-1.5-flash', args={}, api_key_prompt=False):
 
+    # Check availability in eviroment variable, if not found prompt 
+    if api_key_prompt:
+        api_key = api_key_check("GOOGLE_API_KEY")
+    
     GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
     llm = GoogleGenerativeAI(model=model,google_api_key=GOOGLE_API_KEY, **args)
 
